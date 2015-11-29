30fd0c4258537ff699dad1a7adfbfe028f49ccdc
Martin Fernandez <me@bilby91.com>
2015-11-29 17:59:03 -0300

    Add email delivery for new cards

--- models/user.py
+++ models/user.py
@@ -4,3 +4,7 @@ class User(ndb.Model):
     username = ndb.StringProperty();
     email = ndb.StringProperty();
     google_id = ndb.StringProperty();
+
+    @staticmethod
+    def all_emails():
+        return User.query().map(lambda user: user.email)
--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 import os, sys, urllib, jinja2
 
+EMAIL_SENDER = "Ganban <team@ganban.com>"
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
     extensions = ['jinja2.ext.autoescape'],
